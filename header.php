42883d89a4c441aa9224559d89f34f76fed40283
MrMatej <MrMatej@users.noreply.github.com>
2018-03-13 15:50:32 +0000

    Add files via upload
    First

---
+++ header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head><title>ComunQ Computers</title>
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="jquery.matchHeight.js"></script>
+</head>
+<body>
+	<div class="nav">
+		
+<ul>
+	
+<div class="navtop">
+	<li style="float: left">
+	<div class="logoimg"><a href="index.html">
+	<img  class="logo" alt="ComunQ Computers" src="image/logo2.png">
+	<img class="logo2" alt="logo" src="image/logo.png">
+	</a></div></li>
+	<li style="float:left"><a class="active" href="#home">Home</a></li>
+ 	<li style="float:left"><a href="#news">News</a></li>
+ 	<li style="float:left"><a href="#contact">Contact</a></li>
+ 	<li style="float:left"><a href="#about">About</a></li>	
+ 	<div class="account">
+ 	<li class="login" style="float:right"><button onClick="document.getElementById('id01').style.display='block'" style="width:auto;">LOGIN</button></li>	
+ 	<li class="register" style="float:right"><button onClick="" >REGISTER</button></li>	
+	</div>
+	</div>					
+</ul>
+
+	</div>
+<body>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+
+
+
+<html>
+<head><title>ComunQ Computers</title>
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="jquery.matchHeight.js"></script>
+</head>
+<body>
+	<?php include 'header.php';?>
+
+
+	
+	
+
+  
+  
+	
+<div class="para">
+<div class="ptop"> 
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut commodo purus. Pellentesque mattis nec leo ac mattis. Vestibulum tincidunt eleifend nisl, eget commodo urna egestas a. Suspendisse potenti. Donec eleifend rutrum sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla elementum ipsum nec mauris ullamcorper, nec malesuada urna lacinia. Pellentesque vitae ante nunc. Duis hendrerit, quam at ullamcorper euismod, nisi lorem malesuada nisl, sit amet scelerisque magna velit id elit. Etiam facilisis id mi at dictum. Maecenas bibendum, turpis elementum consequat volutpat, lectus ligula bibendum dolor, a dignissim nisi tortor vitae arcu. Phasellus eu lacinia est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent consectetur posuere velit, sed cursus est fermentum a.
+
+Cras at luctus justo. Quisque elit ante, vehicula nec lectus eu, bibendum auctor quam. Quisque eleifend vehicula augue. Phasellus posuere congue arcu eu laoreet. Morbi id enim porta sapien ornare accumsan. Sed efficitur libero eu dui eleifend maximus. Maecenas vel ultrices mi. Mauris quis rhoncus odio, at molestie velit. Morbi iaculis sollicitudin gravida. Vivamus erat erat, malesuada et massa id, laoreet dapibus augue. Sed id justo egestas, hendrerit tellus quis, tincidunt lacus. Sed sagittis erat in eleifend pulvinar. Nullam non turpis nulla. Aenean semper, felis quis lobortis iaculis, dolor mi fringilla nisl, a pharetra tortor quam non urna.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac vulputate dolor. Ut pulvinar augue id quam vestibulum, ac pharetra augue venenatis. Phasellus eget libero tortor. Ut ultricies malesuada lobortis. Aenean at mollis sem. Proin volutpat magna at magna viverra, nec ultrices metus scelerisque. Maecenas id maximus erat. Nullam dapibus eget sem quis ullamcorper. Nunc eget elit aliquam, pretium nibh a, fermentum nisl. Maecenas pretium a risus id venenatis. Sed vel orci id odio mollis varius. Fusce facilisis, urna id hendrerit condimentum, nisl arcu consectetur velit, ac fermentum arcu justo quis felis. Quisque vel arcu eu neque vehicula fringilla id sit amet nibh. Pellentesque quis lectus id turpis porttitor volutpat at sit amet nulla.
+
+Donec bibendum ullamcorper libero, quis iaculis lorem feugiat et. In in finibus quam. Nullam commodo dolor nisi. Aliquam tortor purus, auctor sed finibus sit amet, eleifend vitae orci. Pellentesque convallis cursus hendrerit. Vivamus imperdiet vehicula mauris at feugiat. Vivamus sit amet urna erat. Nam pharetra quis neque cursus viverra. Fusce malesuada, sapien sed rutrum mollis, odio ex molestie neque, ac bibendum felis dolor bibendum nunc. Etiam suscipit leo at lorem ullamcorper suscipit. Nam ac lorem libero. Sed at efficitur diam, eleifend efficitur quam. Nullam nisi odio, egestas et lectus quis, fringilla ornare eros. Pellentesque accumsan ante ac aliquet feugiat. Proin aliquam arcu sit amet ligula commodo, in pellentesque mi dictum. Aliquam congue eu arcu sit amet aliquet.
+
+Etiam tempor ut libero efficitur facilisis. Pellentesque venenatis ipsum mi, sit amet suscipit lorem faucibus ut. Phasellus eget tristique eros, quis fringilla turpis. Nullam imperdiet id augue vitae congue. Donec id quam a magna pellentesque mollis. Proin dui arcu, pulvinar eget magna eu, pretium semper ante. In quis volutpat mi, sed dignissim est.</p>
+
+</div>
+</div>
+<p>HELOO</p>
+
+</body>
+</html>
